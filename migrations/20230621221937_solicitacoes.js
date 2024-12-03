2bd3ab7f8958b44b8826caf191b3351826a62d41
Daniel urbaneki dos santos <daniel_urba@hotmail.com>
2024-12-02 21:14:51 -0300

    Atualização de banco de dados para receber dados de clientes.

--- migrations/20230621221937_solicitacoes.js
+++ migrations/20230621221937_solicitacoes.js
@@ -6,7 +6,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('solicitacoes', table => {
         table.increments('id').primary()
         table.integer('id_usuario').unsigned().references('id').inTable('usuarios').notNullable();  
-        table.integer('id_estrutura').unsigned().references('id').inTable('estruturas').notNullable();   
+        table.integer('id_estrutura').unsigned().references('id').inTable('estruturas').notNullable();
+        table.integer('id_cliente').unsigned().references('id').inTable('pessoas').notNullable();
         table.date('data_inicio').notNull()
         table.date('data_fim')
     })
